bf509bcd4c47779cee1d49a8e79d9cdcff12c246
Margarito Valencia III <52177176+MValenciaIII@users.noreply.github.com>
2024-08-01 15:56:23 -0500

    Problems arrays and conditions finished

--- app.js
+++ app.js
@@ -221,3 +221,169 @@ switch (amHungry == true) {
     default:
         console.log("I just wanna eat to eat.");
 }
+
+
+//!PSUEDOCODE DEBUGGER BREAKING DOWN PROBLEMS
+
+
+//! Favorite Fruit Checker
+//TODO Define an array of favorite fruits
+//? SET array favFruit = [item1, item2,...]
+const favFruits = ["pineapple", "orange", "strawberry", "grapes"]
+//TODO Define a variable for the fruit to check
+//? SET string fruit = "item"
+let fruit = "orange"
+//* STEP 3 Check IF the fruit is in the array 
+//console.log(fruit == favFruits[0]);
+//* and OUTPUT the RESULT
+//? IF statement will be involved
+//? checking to see if VAR fruit is on the VAR favFruit list;
+
+if (fruit == favFruits[0] || fruit == favFruits[1] || fruit == favFruits[2] ||fruit == favFruits[3] ) {
+    console.log("Hey I'm a favorite food!")
+} else {
+    console.log("Yuck, not my taste!")
+}
+// if (fruit == favFruits[0]) {
+//   console.log("Hey I'm a pinapple fruit!")
+
+// } else if(fruit == favFruits[1]) {
+//   console.log("Hey I'm a orange fruit!")
+
+// } else {
+//     console.log("Yuck, not my taste!")
+
+// }
+
+
+//! Grade Categorizer
+//TODO Define a variable for the grade
+//? SET INT gradeNum = 0;
+let gradeNum = 78;
+
+//TODO Categorize the grade
+//? IF statement? 
+//? IF VAR gradeNum is BETWEEN 100 and 90 = A;
+//? ELSE IF VAR gradeNum is BETWEEN 89 and 80 = B; 
+//? ELSE IF VAR gradeNum is BETWEEN 79 and 70 = C; 
+if (gradeNum >= 90 && gradeNum <= 100) {
+    console.log("You've received an A!");
+} else if (gradeNum >= 80 && gradeNum <= 89) {
+    console.log("You've received a B!");
+} else if (gradeNum >= 70 && gradeNum <= 79) {
+    console.log("You've received a C!");
+} else if (gradeNum >= 60 && gradeNum <= 69) {
+    console.log("You've received a D!");
+} else {
+    console.log("You've failed!")
+}
+//* output the result
+//? console.log(GRADE LETTER);
+
+
+
+
+
+//! Day of the Week Checker
+//TODO Define an array of days of the week
+//? SET array daysOfTheWeek = [];
+const daysOfTheWeek = ["sunday","monday","tuesday","wednesday","thursday","friday","saturday"]
+//TODO Define a variable for the day to check
+//? SET string day = "Monday";
+let day = "thursday"
+//TODO Check if the day is a weekday or weekend 
+//? IF statement? 
+//? Conditions: 
+//? - Day of a week
+//? - weekends: "saturday" || "sunday";
+//? - weekdays: "monday" || "tuesday" || "wednesday" || "thursday" || "friday"
+
+switch (day) {
+    case daysOfTheWeek[0]:
+        console.log("It's the weekend! :D");
+        break;
+    case daysOfTheWeek[1]:
+        console.log("It's the Weekday! D:");
+        break;
+    case daysOfTheWeek[2]:
+        console.log("It's the Weekday! D:");
+        break;
+    case daysOfTheWeek[3]:
+        console.log("It's the Weekday! D:");
+        break;
+    case daysOfTheWeek[4]:
+        console.log("It's the Weekday! D:");
+        break;
+    case daysOfTheWeek[5]:
+        console.log("It's the Weekday! D:");
+        break;
+    case daysOfTheWeek[6]:
+        console.log("It's the Weekend! :D");
+        break;    
+    default:
+        console.log("This day doesn't exist??");
+}
+//* output the result
+
+if (day == daysOfTheWeek[0] || day == daysOfTheWeek[6]) {
+    console.log("It's the Weekend! :D");
+} else if (day == daysOfTheWeek[1] || day == daysOfTheWeek[2] || day == daysOfTheWeek[3] || day == daysOfTheWeek[4] || day == daysOfTheWeek[5] ) {
+    console.log("It's the Weekday! D:");
+} else {
+    console.log("This day doesn't exist??");
+}
+
+
+//! Temperature Checker
+//TODO Define a variable for the temperature
+//? SET INT currentTemp = NUM;
+let currentTemp = 70;
+//TODO Categorize the temperature and 
+//? If Structure
+//? 3 conditions:
+//? HOT - currentTemp >= 85
+//? just right - currentTemp < 85 AND currentTemp <= 60
+//? COLD - currentTemp < 60
+
+if (currentTemp >= 85) {
+    console.log("Aye it's way too HOT brother!");
+} else if (currentTemp < 85 && currentTemp >= 60) {
+    console.log("Keep it like this Dude! Just right!");
+} else {
+    console.log("I'm freezing. Global warming isn't real!")
+}
+//*output the result
+
+
+//! Number Sign Checker
+//TODO Define a variable for the number
+//? SET INT num = NUMBER;
+let num = 0;
+//TODO Check the sign of the number
+//? Switch Statement
+//? 3 conditions:
+//? If num > 0
+//? If num == 0;
+//? if num < 0;
+debugger
+switch (true) {
+    case (num > 0):
+        console.log("THis is a positive Number!");
+        break;
+    case (num == 0):
+        console.log("This is a zero number");
+        break;
+    case (num < 0):
+        console.log("This number is negative!");
+        break;
+}
+
+if (num > 0 ) { 
+    console.log("THis is a positive Number!");
+} else if (num === 0) {
+    console.log("This is a zero number")
+} else if (num < 0) {
+    console.log("This number is negative!");
+}
+
+//* output the result
